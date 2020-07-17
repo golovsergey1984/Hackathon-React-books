55343a8901047dc817f4d623d824a4a388b9231f
Volodymyr Kheroim <volodymyr_kh@hotmail.com>
2020-07-18 02:48:39 +0300

    Header main

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import Header from './Header/header';
 
 class App extends Component {
-
-
-    render() {
-
-
-        return (
-
-            <div>
-                Hello world
-            </div>
-
-
-        );
-    }
-
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = state => ({});
 
-})
+const mapDispatchToProps = dispatch => ({});
 
-const mapDispatchToProps = (dispatch) => ({
-
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/components/Header/header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import styles from './header.module.css';
+
+// const activeStyle = {
+//   color: 'pale',
+// };
+
+const Header = () => (
+  <header className={styles.header}>
+    <nav className={styles.navigation}>
+    <a href="/" className={styles.siteLogo}>BR</a>
+    <p className={styles.name}><span className={styles.mStyle}>M</span> Martha Stewart</p>
+      <ul className={styles.navigationList}>
+        <li className={styles.navigationListItem}>
+          {/* <Link to="/" exact activeStyle={activeStyle} className={styles.Link} > */}
+          <a href="/library"> </a>
+          {/* </Link> */}
+        </li>
+        <li className={styles.navigationListItem}>
+          {/* <Link to="/movies" activeStyle={activeStyle} className={styles.Link}> */}
+          <a href="/statistics"> </a>
+          {/* </Link> */}
+        </li>
+        <li className={styles.navigationListItem}>
+          {/* <Link to="/movies" activeStyle={activeStyle} className={styles.Link}> */}
+          {/* <a href="#"> exit </a> */}
+          <button type='button' className={styles.exitButton}><span className={styles.exitButtonTitle}>Вихід</span></button>
+          {/* </Link> */}
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
