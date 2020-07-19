bbac5c65a1bc0dd5ea09f7f04b9ae026519e86c3
Mykhail1387 <melnychukmykhailo87@gmail.com>
2020-07-19 14:02:16 +0300

    commit one

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import LogOut from "../pages/LogOut";
 
+
 class App extends Component {
   render() {
     return (
       <div>
         <p>Hello world!!!</p>
         {/* <LogOut /> */}
+
       </div>
     );
   }
--- src/pages/LogOut.jsx
+++ src/pages/LogOut.jsx
@@ -1,46 +1,51 @@
-import React, { Component } from "react"
-import style from "../styles/LogOut.module.css"
-import { Link } from "react-router-dom";
-
+import React, { Component } from 'react'
+import style from '../styles/LogOut.module.css'
+import { Link } from 'react-router-dom'
 
 class LogOut extends Component {
-
-  componentDidMount() {
-    window.addEventListener("keydown", this.onCnacelPress)
+  componentDidMount () {
+    window.addEventListener('keydown', this.onCnacelPress)
   }
 
-  onCancelClick = (event) => {
-    if (event.target.id !== "cancel" && event.target !== event.currentTarget) return
+  onCancelClick = event => {
+    if (event.target.id !== 'cancel' && event.target !== event.currentTarget)
+      return
 
-    console.log("Exit!!")
+    console.log('Exit!!')
   }
 
-  onCnacelPress = (e) => {
-    if (e.key !== "Escape") return
+  onCnacelPress = e => {
+    if (e.key !== 'Escape') return
 
-    console.log("Exit key!!!")
+    console.log('Exit key!!!')
   }
 
   onLogout = () => {
-    console.log("Log out and go main page")
+    console.log('Log out and go main page')
   }
 
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.onCnacelPress)
+  componentWillUnmount () {
+    window.removeEventListener('keydown', this.onCnacelPress)
   }
 
-  render() {
+  render () {
     return (
-      <div className={style.logOut_wrapper} onClick={(event) => this.onCancelClick(event)}>
+      <div
+        className={style.logOut_wrapper}
+        onClick={event => this.onCancelClick(event)}
+      >
         <div className={style.logOut_box}>
-          <p className={style.logOut_box_descr}>Якщо Ви вийдете з програми незбережені дані будуть втрачені</p>
+          <p className={style.logOut_box_descr}>
+            Якщо Ви вийдете з програми незбережені дані будуть втрачені
+          </p>
           <div className={style.logOut_box_dtns}>
-            <button className={style.btn_cancel} id={"cancel"}>Відміна</button>
+            <button className={style.btn_cancel} id={'cancel'}>
+              Відміна
+            </button>
 
             {/* <Link to="/login">
               <button className={style.btn_logout} onClick={this.onLogout}>Вийти</button>
             </Link> */}
-
           </div>
         </div>
       </div>
