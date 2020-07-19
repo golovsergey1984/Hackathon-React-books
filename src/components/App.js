053107a555b54299e649e1012da0a2636909fe23
S.Lazarenko <ajaxamsterdam878@gmail.com>
2020-07-20 00:19:05 +0300

    init Lazarenko_Sergey

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { lazy, Suspense, Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Loader from 'react-loader-spinner';
+import Form_Library from './library_addBooks/Form/Form_Library.js'
 import './main.module.css';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import Library_addBooks from './library_addBooks/Library_addBooks.js';
@@ -10,6 +11,7 @@ import products from './library_addBooks/data.json';
 
 const App = () => (
   <div>
+    <Form_Library/>
     <Library_addBooks>
       <LibraryList items={products} />
     </Library_addBooks>
