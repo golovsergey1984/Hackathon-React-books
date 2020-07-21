35ce904a2fc470099d3f9aaad4ea05ccc5a591c5
Volodymyr Kheroim <volodymyr_kh@hotmail.com>
2020-07-21 16:21:14 +0300

    style commits to Staistic page

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -6,13 +6,8 @@ import styles from './header.module.css';
 const Header = () => (
   <header className={styles.header}>
     <nav className={styles.navigation}>
-<<<<<<< HEAD
     <a href="/login" className={styles.siteLogo}>BR</a>
     <p className={styles.name}><span className={styles.mStyle}>M</span> Martha Stewart</p>
-=======
-      <a href="/" className={styles.siteLogo}>BR</a>
-      <p className={styles.name}><span className={styles.mStyle}>M</span> Martha Stewart</p>
->>>>>>> dev
       <ul className={styles.navigationList}>
         <li className={styles.navigationListItem}>
           <Link to="/library" className={styles.link}>
@@ -24,13 +19,8 @@ const Header = () => (
 
         </li>
         <li className={styles.navigationListItem}>
-<<<<<<< HEAD
           <button type='button' className={styles.exitButton}><span className={styles.exitButtonTitle}>Вихід</span></button>
           {/* <Link to="/logout" className={styles.exitButton}><span className={styles.exitButtonTitle}>Вихід</span> </Link> */}
-=======
-          {/* <button type='button' className={styles.exitButton}><span className={styles.exitButtonTitle}>Вихід</span></button> */}
-          <Link to="/login" className={styles.exitButton}><span className={styles.exitButtonTitle}>Вихід</span> </Link>
->>>>>>> dev
         </li>
       </ul>
     </nav>
