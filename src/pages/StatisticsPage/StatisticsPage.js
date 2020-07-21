7cd1377ddf2353e6f7d41bad37dc2b6689ba867c
Volodymyr Kheroim <volodymyr_kh@hotmail.com>
2020-07-21 14:30:14 +0300

    pull from dev

--- src/pages/StatisticsPage/StatisticsPage.js
+++ src/pages/StatisticsPage/StatisticsPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import LineChart from '../../components/LineChart/LineChart';
+import Statistic from '../../components/Statistic/Statistic.jsx'
 
 export default class StatisticsPage extends Component {
   render() {
     return (
       <>
-        <div>STATISTICS PAGE</div>
+        {/* <div>STATISTICS PAGE</div> */}
+        <Statistic/>
         <LineChart />
       </>
     );
