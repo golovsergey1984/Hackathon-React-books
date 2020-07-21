baa693e742d8a34b685d581d98facd28df477b3d
golovsergey1984 <51264173+golovsergey1984@users.noreply.github.com>
2020-07-21 15:42:53 +0300

    library empty added

--- src/pages/LibraryPage/LibraryPage.js
+++ src/pages/LibraryPage/LibraryPage.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 
 //Components
+import Library_empty from "../../components/EmptyList/EmptyList";
 import Library_addBook from "../../components/library_addBooks/Library_addBooks"
 import Library_all_categories from "../../components/library/BookList/Library_all_categories"
 
@@ -9,6 +10,7 @@ export default class LibraryPage extends Component {
   render() {
     return (
       <Fragment>
+        <Library_empty />
         <Library_addBook />
         <Library_all_categories />
       </Fragment>
