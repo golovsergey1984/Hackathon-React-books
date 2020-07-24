2475f9ca043404f17418af9b6ddcdd4a54eceb46
warbonnet333 <warbonnet333@gmail.com>
2020-07-24 11:54:12 +0300

    added same styles to login form and registration

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,17 +1,11 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 // import { NavLink } from 'react-router-dom';
 import withAuthRedirect from '../../hoc/withAuthRedirect';
 import LoginForm from '../../components/LoginForm/LoginForm';
-import LogOutModal from "../../components/LogOutModal/LogOutModal"
 
 class LoginPage extends Component {
   render() {
-    return (
-      <Fragment>
-        <LoginForm />
-        {/* <LogOutModal /> */}
-      </Fragment>
-    );
+    return <LoginForm />;
   }
 }
 
