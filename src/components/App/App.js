83ae9f8cc62dcf5173d219d931b8d3ebba6c8ef3
golovsergey1984 <51264173+golovsergey1984@users.noreply.github.com>
2020-07-26 19:59:53 +0300

    statistic added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 // Components
 import Loader from 'react-loader-spinner';
 import Header from '../Header/header';
-/* import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'; */
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 import { getUserAction } from '../../redux/session/sessionActions';
 //Styles
 import styles from './App.module.css';
@@ -67,17 +67,17 @@ class App extends Component {
           <Switch>
             <Route path="/login" component={AsyncLoginPage} />
             <Route path="/registration" component={AsyncRegistrationPage} />
-            <Route
+            <ProtectedRoute
               path="/library"
               component={AsyncLibraryPage}
               redirectTo="/login"
             />
-            <Route
+            <ProtectedRoute
               path="/training"
               component={AsyncTrainingPage}
               redirectTo="/login"
             />
-            <Route
+            <ProtectedRoute
               path="/statistics"
               component={AsyncStatisticsPage}
               redirectTo="/login"
