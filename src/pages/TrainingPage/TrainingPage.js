e85793c1fd998e47ce414de6eaac3e5331caad83
Volodymyr Kheroim <volodymyr_kh@hotmail.com>
2020-07-27 22:25:25 +0300

    push training data to server

--- src/pages/TrainingPage/TrainingPage.js
+++ src/pages/TrainingPage/TrainingPage.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import Training from '../../components/Training/Training';
 
 export default class TrainingPage extends Component {
-  componentDidMount(){
-    const { getBooksAction } = this.props;
-    getBooksAction();
+  componentDidMount() {
+    this.props.getAllBooks();
   }
   render() {
-    console.log("Props: ", this.props);
-    console.log("State: ", this.state);
+    // console.log("Props: ", this.props);
+    
     return (
-      <Training />
+      <Training {...this.props}/>
     );
   }
 }
--- src/pages/TrainingPage/TrainingPageContainer.js
+++ src/pages/TrainingPage/TrainingPageContainer.js
@@ -1,15 +1,20 @@
 import TrainingPage from './TrainingPage';
 import { connect } from 'react-redux';
 import { getPlannedBooks } from '../../redux/books/booksSelectors';
-import {getBooksAction} from '../../redux/books/booksActions'
+import {getBooksAction} from '../../redux/books/booksActions';
+import {getTrainingId} from '../../redux/training/trainingSelectors';
 
 const mapStateToProps = state => ({
   plannedBooks: getPlannedBooks(state),
+  trainingId: getTrainingId(state),
 });
 
-const mapDispathToProps = () => ({
-    // getAllBooks: () => dispatch(getBooksAction()),
-    getBooksAction
+const mapDispathToProps = dispatch => ({
+    getAllBooks: () => dispatch(getBooksAction()),
   });
 
 export default connect(mapStateToProps, mapDispathToProps)(TrainingPage);
+
+  //TrainingPage
+   
+  
